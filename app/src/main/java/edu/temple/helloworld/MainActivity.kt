c98e8127cf7312e0dedf527796685f12d1a3c7d8
Leo <leo.battalora@gmail.com>
2022-01-27 09:49:07 -0500

    added button to change displayed text

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -1,11 +1,20 @@
 package edu.temple.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // change textview text when button is clicked
+        val textView = findViewById<TextView>(R.id.textview)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            textView.text = "You clicked the button"
+        }
     }
 }
